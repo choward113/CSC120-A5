bde641892841516c602f050fe5cfd8d065211092
Camille Howard <choward@smith.edu>
2023-03-04 17:42:22 -0500

    Fixed getCar

--- Engine.java
+++ Engine.java
@@ -29,21 +29,13 @@ public class Engine {
 
 
     public void go(){
-        if (this.currentFuel > 0) {
+        if ((this.currentFuel - 20) > 0) {
            this.currentFuel = this.currentFuel - 20;
         } else {
-            throw new RuntimeException("Not enough fuel. Fuel level is: "+ this.currentFuel);
+            throw new RuntimeException("Not enough fuel. Fuel level is: "+ this.currentFuel +". Needs "+ (20-this.currentFuel)+ " more fuel.");
         }
         System.out.println("Going! Remaining fuel: "+ this.currentFuel);
     }
 
     
 }
-
-/*a private `FuelType` attribute to indicate what type of fuel it uses, and `
-    double`s to store the current and maximum fuel levels (along with an approproate accessors for each)
-    - a constructor, which takes in initial values for the attributes named above and sets them appropriately
-    - a method `public void refuel()` which will reset the `Engine`'s current fuel level to the maximum
-    - a method `public void go()` which will decrease the current fuel level and 
-    print some useful information (e.g. remaining fuel level) provided the fuel level is above 0 
-    (otherwise it should throw a `RuntimeException` containing an informative message)*/
--- Train.java
+++ Train.java
@@ -16,7 +16,6 @@ public class Train {
         this.nCars = nCars;
         this.passengerCapacity = passengerCapacity;
         this.engine = new Engine(fuelType, fuelCapacity);
-    
     }
 
     public Engine getEngine(){
@@ -30,14 +29,14 @@ public class Train {
         } else {
             throw new RuntimeException("This car has already been added to the train.");
         }
-        
     }
 
     public Car getCar(int i){ /**Test this*/
-        if (i+1 <= this.carList.size()){
-            System.out.println("Car "+ (i) +this.carList.get(i-1).toString());
+        if (i <= this.carList.size()){
+            System.out.print("Car "+ (i) +": ");
             return this.carList.get(i-1);
         } else {
+
             throw new RuntimeException("Car "+ i+ " does not exist");
         }
     }
@@ -51,6 +50,7 @@ public class Train {
         }
         return maxCapacity;
     }
+    
     public int seatsRemaining(){
         int totalRemaining = 0;
         for (int i = 0; i < nCars; i++){
@@ -67,46 +67,4 @@ public class Train {
             carList.get(i).printManifest();  
         }
     }
-    public static void main(String[] args) {
-        Train train = new Train(FuelType.ELECTRIC, 100, 2, 2);
-        Car car1 = new Car(1);
-        Car car2 = new Car(2);
-        train.addCar(car1);
-        train.addCar(car2);
-        
-        Passenger p = new Passenger("John");
-        car1.addPassenger(p);
-        Passenger p2 = new Passenger("Mary");
-        Passenger p3 = new Passenger("Lucy");
-        car2.addPassenger(p2);
-        car2.addPassenger(p3); 
-
-        train.printManifest(); //Test manifest
-        System.out.println("Max Capacity: "+train.getMaxCapacity()); //test maxCapacity
-        try {
-            while (true) {
-                train.engine.go();
-            }
-        } catch (Exception e) {
-            System.err.println(e.getMessage()); // Out of fuel
-        }
-        System.out.println("Seats remaining: "+ train.seatsRemaining());
-        train.getEngine();
-        train.getCar(1);
-    }
 }
-
-/**
- *  -  a private `Engine` attribute, which we will mark with the keyword `final` to establish the 
- * **composition** relationship (e.g. `private final Engine engine;`)
- -  a private `ArrayList` to keep track of the `Car`s currently attached
- -  a constructor `public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity)` 
- which will initialize the `Engine` and `Car`s and store them
- -  a couple of accessors: 
-     -  `public Engine getEngine()`
-     -  `public Car getCar(int i)` to return the `i`th car
-     -  `public int getMaxCapacity()` which will return the maximum total capacity across all `Car`s
-     -  `public int seatsRemaining()` which will return the number of remaining open seats across all `Car`s 
-     -  `public void printManifest()` that prints a roster of all `Passenger`s onboard (_Hint: your `Car`s can help!_)
-
- */
